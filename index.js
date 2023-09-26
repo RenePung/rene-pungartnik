c83484f6e032cf9bcf18400a68e5c7705742851a
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-09-26 22:20:48 +0200

    added animation
    added color change on scroll to navbar

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ typewriter.typeString('Welcome!')
     .pauseFor(250000000000)
     .deleteAll()
     .start();
-//type WritterEffect
+//Type WritterEffect
+
+
+// Bootstrap navbar change color on scroll
+const navEl = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+if (window.scrollY >= 56) {
+navEl.classList.add('navbar-scrolled')
+} else if (window.scrollY < 56) {
+    navEl.classList.remove('navbar-scrolled');
+}
+});
+// Bootstrap navbar change color on scroll
